62808d3e18e58fcd98cd8800643253ea5a55b347
DalioSY <dalioyamada@gmail.com>
2024-01-15 14:38:43 -0300

    display-flex

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-let p1 = document.querySelector('p')
+let p1 = document.querySelector('.p1')
 
-function generateNumber(){
-   
+function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
-    if (max > min){
-        p1.innerHTML =`The drawn number is ${Math.floor(Math.random() * (max - min + 1)) + min}` ;
-    
-    }else {
-        alert("The minimum value must be LESS than the maximum value")
+    if (max > min) {
+        p1.innerHTML = Math.floor(Math.random() * (max - min + 1)) + min;
+        p1.style.color = "green";
     }
-
+    if (max === min) {
+        p1.innerHTML = "minimum number must be DIFFERENT from the maximum number";
+        p1.style.color = "yellow";
+    }
+    if (max < min) {
+        p1.innerHTML = "The minimum value must be LESS than the maximum value";
+        p1.style.color = "red";
+    }
 }
